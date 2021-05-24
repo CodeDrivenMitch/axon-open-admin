043a71ea679ad1586852b1ec990dcafe11e91561
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2021-05-24 14:04:30 +0200

    Add AutoConfiguration classes again

---
+++ starter/src/main/java/com/insidion/autoconfigure/AxonOpenAdminAutoConfiguration.kt
@@ -0,0 +1,14 @@
+package com.insidion.autoconfigure
+
+import com.insidion.axon.openadmin.AxonAdminConfiguration
+import org.axonframework.springboot.autoconfig.AxonAutoConfiguration
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration(proxyBeanMethods = false)
+@EnableScheduling
+@Import(AxonAdminConfiguration::class)
+@AutoConfigureAfter(AxonAutoConfiguration::class)
+class AxonOpenAdminAutoConfiguration
